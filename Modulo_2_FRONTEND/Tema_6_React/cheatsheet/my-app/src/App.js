75070c02c4c2284d3e42c9a9daefd7c911c6e937
Lorena Lozano <lorenalv93@hotmail.com>
2022-08-04 15:18:48 +0200

    Nuevo Proyecto React

---
+++ Modulo_2_FRONTEND/Tema_6_React/cheatsheet/my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import FirstComponents from './components/FirstComponents';
+
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponents />
+      <FirstComponents />
+      <FirstComponents />
+      <FirstComponents />
+    </div>
+  );
+}
+
+export default App;
---
+++ Modulo_2_FRONTEND/Tema_6_React/cheatsheet/my-app/src/components/FirstComponents.js
@@ -0,0 +1,11 @@
+export default function FirstComponents() {
+  return (
+    <div>
+        <h3>Título</h3>
+        <small>16 de Agosto de 2025</small>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum cumque, velit beatae impedit blanditiis repellat nisi eum dolor fuga porro fugit facilis facere atque placeat sequi, nobis architecto animi ratione.</p>
+    </div>
+  )
+}
+
+//Alternativa: export default FirstComponents;
