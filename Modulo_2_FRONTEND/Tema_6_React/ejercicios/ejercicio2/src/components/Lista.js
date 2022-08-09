6a18fcf09ec65964905bd5190c8802d3d4cd9cfb
Lorena Lozano <lorenalv93@hotmail.com>
2022-08-09 10:45:15 +0200

    Ejercicio 2 terminado con React

--- Modulo_2_FRONTEND/Tema_6_React/ejercicios/ejercicio2/src/components/Lista.js
+++ Modulo_2_FRONTEND/Tema_6_React/ejercicios/ejercicio2/src/components/Lista.js
@@ -1,8 +1,38 @@
 export default function Lista(props) {
-
-    return (
+  return (
     <>
-      <h2>{props.categoria}</h2>  
+      <h2>{props.categoria}</h2>
+      <ul className = "list-group">
+        {/*Ponemos el json para que lo que venga de productos, que es objetos se pueda imprimir en pantalla*/}
+        {props.productos.map(product => {
+          return (<li key = {product.id} className = "list-group-item">
+                      {/*Para que no aparezca el warning en la consola, es porque con el map
+                       hay que hacer una cosa. El elemento que este por encima de todo, en este caso el unico
+                       que tenemos el <li key = {name + modelo + precio}> esa key tiene que ser unico y tiene que ser cualquier tipo de dato
+                       que no se va a repetir*/}
+                      {product.id} {product.name} {product.marca}. precio:{product.precio} 
+                  </li>)
+        })}
+      </ul>    
     </>
   )
 }
+
+/* DESESTRUCTURACION DE OBJETOS / ASIGNACIÓN AVANZADA
+export default function Lista(props) {
+  
+  const {(categoria, productos)} = props;
+  console.log(categoria, productos);
+
+  return (
+  <>
+    <h2>{categoria}</h2>
+    <ul>
+      {props.productos.map(({id, name, modelo, precio })=> {
+        return (<li>{id} {name} {marca}. precio:{precio} </li>)
+      })}
+    </ul> 
+     
+  </>
+)
+}*/
--- Modulo_2_FRONTEND/Tema_6_React/ejercicios/ejercicio2/src/components/Tienda.js
+++ Modulo_2_FRONTEND/Tema_6_React/ejercicios/ejercicio2/src/components/Tienda.js
@@ -1,30 +1,29 @@
-import Lista from "./Lista"; /*No hemos puesto component en la ruta porque esta al mismo nivel*/
+import Lista from "./Lista"; /*No hemos puesto components en la ruta porque esta al mismo nivel*/
 
 export default function Tienda() {
     const tienda = {
       electronica:[
-        {id: 27, producto: "Televisor", marca: "LG", modelo: "XP7302", precio: 399},
-        {id: 28, producto: "Equipo Hi-Fi", marca: "Samsung", modelo: "VF235", precio: 179},
-        {id: 29, producto: "Televisor", marca: "Sony", modelo: "Bravia-173", precio: 498},
+        {id: 27, name: "Televisor", marca: "LG", modelo: "XP7302", precio: 399},
+        {id: 28, name: "Equipo Hi-Fi", marca: "Samsung", modelo: "VF235", precio: 179},
+        {id: 29, name: "Televisor", marca: "Sony", modelo: "Bravia-173", precio: 498},
       ],
       alimentacion: [
-        {id: 30, producto: "Galletas", marca: "María" , precio: 0.90},
-        {id: 31, producto: "Ensalada", marca: "Imizurra" , precio: 1.30},
-        {id: 32, producto: "Patatas", marca: "McKain", precio: 0.90},
-        {id: 33, producto: "Pasta", marca: "Gallo", precio: 0.90},
+        {id: 30, name: "Galletas", marca: "María" , precio: 0.90},
+        {id: 31, name: "Ensalada", marca: "Imizurra" , precio: 1.30},
+        {id: 32, name: "Patatas", marca: "McKain", precio: 0.90},
+        {id: 33, name: "Pasta", marca: "Gallo", precio: 0.90},
       ],
       mascotas: [
-        {id: 34, producto: "Croquetas para gato", marca: "Purina" , precio: 4.90},
-        {id: 35, producto: "Arena de gato", marca: "Limpior", precio: 1.10},
+        {id: 34, name: "Croquetas para gato", marca: "Purina" , precio: 4.90},
+        {id: 35, name: "Arena de gato", marca: "Limpior", precio: 1.10},
       ]
     };
 
   return (
     <div>
-     <Lista 
-      categoria = "Electronica" 
-     
-    />
+      <Lista categoria = "Electronica" productos= {tienda.electronica}/>
+      <Lista categoria = "Alimentación" productos= {tienda.alimentacion}/>
+      <Lista categoria = "Mascotas" productos= {tienda.mascotas}/>
     </div>
   )
 }
